8802adaf277c20d64bb4fa087cb4701cd4011fa4
jward <wardja2@appstate.edu>
2017-10-07 16:27:03 -0400

    Creating Basic File Structure

--- mAppQuiz/mAppQuiz/SignInPages/LoginPage.xaml.cs
+++ mAppQuiz/mAppQuiz/SignInPages/LoginPage.xaml.cs
@@ -15,8 +15,8 @@ namespace mAppQuiz
 #pragma warning disable CS0618 // Type or member is obsolete
             logo.Source = Device.OnPlatform(
                 iOS: ImageSource.FromResource("appealing_logo.png"),
-                Android: ImageSource.FromFile("appealing_logo.png"),
-                WinPhone: ImageSource.FromFile("Images/logical_logo.png"));
+                Android: ImageSource.FromFile("logical_logo.png"),
+                WinPhone: ImageSource.FromFile("Images/appealing_logo.png"));
 #pragma warning restore CS0618 // Type or member is obsolete
         }
 
