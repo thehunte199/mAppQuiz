97e3ef7c5e3f115d5859ba170381cb736512df1e
thehunte199 <thehunte199@users.noreply.github.com>
2017-10-30 09:17:08 -0400

    Update HomePage.xaml.cs

--- mAppQuiz/mAppQuiz/ContentPages/HomePage.xaml.cs
+++ mAppQuiz/mAppQuiz/ContentPages/HomePage.xaml.cs
@@ -20,7 +20,7 @@ namespace mAppQuiz
             await DisplayAlert("wee", "woo", "blah");
         }
 	
-	aync void CreateClass(object sender, EventArgs e)
+	async void CreateClass(object sender, EventArgs e)
 	{
 	    await DisplayAlert("I", "AM", "THE", "WALRUS");
 	}
