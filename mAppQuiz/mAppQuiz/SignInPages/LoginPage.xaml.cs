ce551ab99cae46261bb6ffc5e3595a76e4853cef
jward <wardja2@appstate.edu>
2017-12-06 21:03:53 -0500

    Changed Android to have alternate logo

--- mAppQuiz/mAppQuiz/SignInPages/LoginPage.xaml.cs
+++ mAppQuiz/mAppQuiz/SignInPages/LoginPage.xaml.cs
@@ -17,7 +17,7 @@ namespace mAppQuiz
 #pragma warning disable CS0618 // Type or member is obsolete
             logo.Source = Device.OnPlatform(
                 iOS: ImageSource.FromResource("appealing_logo.png"),
-                Android: ImageSource.FromFile("logical_logo.png"),
+                Android: ImageSource.FromFile("appealing_logo.png"),
                 WinPhone: ImageSource.FromFile("Images/logical_logo.png"));
 #pragma warning restore CS0618 // Type or member is obsolete
         }
